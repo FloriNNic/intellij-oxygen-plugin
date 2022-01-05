7952eb3c06f86744153e2eab513b2bc8e9d197b1
GitHub Action <action@github.com>
2022-01-05 10:31:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/florinnic/intellijoxygenplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.florinnic.intellijoxygenplugin.services
+
+import com.github.florinnic.intellijoxygenplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
